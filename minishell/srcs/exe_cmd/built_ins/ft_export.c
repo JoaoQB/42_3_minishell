4f37380a7038ecb76bf05497c483002ae8ce833b
Joao Luis Queijo Barroco Sobral <jqueijo-@c2r6s4.42lisboa.com>
2024-09-24 15:25:10 +0100

    feat(export): implements export var+=

--- minishell/srcs/exe_cmd/built_ins/ft_export.c
+++ minishell/srcs/exe_cmd/built_ins/ft_export.c
@@ -6,11 +6,39 @@
 /*   By: jqueijo- <jqueijo-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 12:51:31 by jqueijo-          #+#    #+#             */
-/*   Updated: 2024/09/20 16:03:19 by jqueijo-         ###   ########.fr       */
+/*   Updated: 2024/09/24 15:18:20 by jqueijo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/minishell.h"
+
+static char	*export_append_values(t_env *new, t_env *current)
+{
+	char	*new_value;
+	char	*new_var_value;
+	int		len;
+
+	if (!new || !new->value || !current)
+		return (NULL);
+	if (current->var_value && !new->var_value)
+		return (extract_from_i(current->var_value, 0));
+	new_var_value = NULL;
+	len = ft_strlen(new->var_value) + ft_strlen(current->var_value);
+	new_var_value = (char *)safe_malloc(sizeof(char) * (len + 1));
+	if (!new_var_value)
+		return (NULL);
+	new_var_value[0] = '\0';
+	if (current->var_value)
+		ft_strcat(new_var_value, current->var_value);
+	if (new->var_value)
+		ft_strcat(new_var_value, new->var_value);
+	new_value = env_get_value(new->var, new_var_value);
+	ft_free(&current->value);
+	current->value = new_value;
+	ft_free(&current->var_value);
+	current->var_value = new_var_value;
+	return (extract_from_i(new_var_value, 0));
+}
 
 static void	handle_not_found(t_main *main_s, t_env *new)
 {
@@ -34,12 +62,17 @@ static void	handle_found(t_main *main_s, t_env *new, t_env *current)
 
 	if (!main_s || !new || !new->value || !current)
 		return ;
+	var_value = NULL;
 	if (!find_equal(new->value))
 		return ;
-	var_value = NULL;
+	else if (find_plus(new->value))
+	{
+		var_value = export_append_values(new, current);
+		ft_setenv(main_s, new->var, var_value, 1);
+		return ;
+	}
 	ft_free(&current->value);
-	if (new->value)
-		current->value = extract_from_i(new->value, 0);
+	current->value = extract_from_i(new->value, 0);
 	ft_free(&current->var_value);
 	if (new->var_value)
 	{
