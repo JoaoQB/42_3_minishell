e10e4ad9571db4c0e98ada661212591613f06634
Filipe Andre Barata Antunes <fandre-b@c2r1s1.42lisboa.com>
2024-10-08 17:40:35 +0100

    special cases not piping

--- minishell/srcs/exe_cmd/exe_cmd.c
+++ minishell/srcs/exe_cmd/exe_cmd.c
@@ -6,7 +6,7 @@
 /*   By: fandre-b <fandre-b@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 15:51:15 by fandre-b          #+#    #+#             */
-/*   Updated: 2024/10/08 17:28:08 by fandre-b         ###   ########.fr       */
+/*   Updated: 2024/10/08 17:39:54 by fandre-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,14 +72,15 @@ void	ft_exe_pipex_s(void)
 	t_pipex	*pipex_s;
 
 	pipex_s = minishell()->pipex;
-	if (!check_for_pipeline()) //handle no pipeline
-		return ; //this worked but i did simplier
+	// if (!check_for_pipeline()) //handle no pipeline //TODO exit
+	// 	return ; //this worked but i did simplier
+	if (pipex_s->cmd)
 	while (pipex_s)
 	{
 		pipex_s->pid = fork();
 		if (pipex_s->pid == -1)
 			return (perror("fork failed")); //TODO Handle error s
-		else if (pipex_s->pid == 0)
+		else if (pipex_s->pid == 0 && !special_edge_cases(pipex_s))
 			exe_cmd_child(pipex_s, minishell()->menv);
 		// else <parent> change stuff here latter
 		//	exe_cmd_parent()
