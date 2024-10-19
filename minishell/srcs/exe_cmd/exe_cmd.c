30b0697c5b6dc6c53c8cec477a94a4622c027fb3
fandre-b <fandre-b@student.42.fr>
2024-10-19 15:56:08 +0100

    Reverted

--- minishell/srcs/exe_cmd/exe_cmd.c
+++ minishell/srcs/exe_cmd/exe_cmd.c
@@ -6,7 +6,7 @@
 /*   By: fandre-b <fandre-b@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 15:51:15 by fandre-b          #+#    #+#             */
-/*   Updated: 2024/10/17 22:52:41 by fandre-b         ###   ########.fr       */
+/*   Updated: 2024/10/19 15:55:51 by fandre-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,7 +66,7 @@ void	exe_cmd_child(t_pipex *pipex_s, char **envp)
 		ft_exit_pid(pipex_s);
 	pipex_s->status = ft_n_update_path(pipex_s);
 	if (pipex_s->status || !pipex_s->path)
-		ft_exit_pid(pipex_s);
+		ft_exit_pid(pipex_s); 
 	else if (execve(pipex_s->path, pipex_s->cmd, envp) == -1)
 		pipex_s->status = errno;
 	ft_exit_pid(pipex_s);
